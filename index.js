d6a49334355ab29a73442f7fbcd305d594370592
Jahid <hzahid905@gmail.com>
2025-07-09 18:20:33 +0600

    api to create user

--- index.js
+++ index.js
@@ -25,8 +25,42 @@ const client = new MongoClient(process.env.MONGODB_URI, {
 
 async function run() {
   try {
+    // databases
+    const db = client.db("edusyncDB");
+    const usersCollection = db.collection("users");
+
+    // create user
+    app.post("/api/registerUser", async (req, res) => {
+      try {
+        const { email, role, lastSignIn } = req.body;
+
+        // ✅ Basic validation
+        if (!email || !role || !lastSignIn) {
+          return res.status(400).json({ message: "Missing required fields" });
+        }
+
+        const userExists = await usersCollection.findOne({ email });
+        if (userExists) {
+          return res.status(409).json({ message: "User already exists" });
+        }
+
+        const newUser = {
+          email,
+          role,
+          CreatedAt: new Date().toISOString(),
+          lastSignIn,
+        };
+        const result = await usersCollection.insertOne(newUser);
+        if (!result.insertedId) {
+          return res.status(401).json({ message: "Registration failed" });
+        }
+        res.status(200).send(result);
+      } catch (error) {
+        console.log(error.message);
+      }
+    });
     // await client.db("admin").command({ ping: 1 });
-    console.log("connected to mongodb");
+    // console.log("connected to mongodb");
   } catch (error) {
     console.log(error);
   } finally {
